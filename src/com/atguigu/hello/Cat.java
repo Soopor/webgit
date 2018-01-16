6cb3b47b77c9d86c2cacf0999b3780e837aae07c
TreeMapLink <zonghaixin_zte@163.com>
2018-01-16 20:11:25 +0800

    01

--- src/com/atguigu/hello/Cat.java
+++ src/com/atguigu/hello/Cat.java
@@ -3,6 +3,6 @@ package com.atguigu.hello;
 public class Cat {
 	public static void main(String[] args) {
 		System.out.println("hello");
-		System.out.println("cat")
+		System.out.println("cat");
 	}
 }
